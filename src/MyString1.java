05afa08c2b38b62d1d6e3015134bce646f892d50
LivSmitty <livia@jansenresearch.com>
2016-09-25 16:44:41 -0700

    Commit to GIT

---
+++ src/MyString1.java
@@ -0,0 +1,44 @@
+
+/**
+ * Created by scott_000 on 9/25/2016.
+ */
+public class MyString1 {
+
+
+    public MyString1(char[] chars){
+        String message = new String(chars);
+    }
+
+    public char charAt(int index){
+        chars = message.index();
+    }
+
+
+    public int length(){
+        int length = message.length;
+    }
+
+
+    public MyString1 substring(int begin, int end){
+
+        Strinbuilder message = new Stringbuilder();
+        message.substring(begin, end);
+    }
+
+    public MyString1 toLowerCase(){
+
+        String message = message.text.toLowerCase;
+    }
+
+    public boolean equals(MyString1 s){
+
+    }
+
+    public static MyString1 valueOf(int i){
+       int value = MyString1.valueOf(i);
+    }
+
+}
+
+
+
